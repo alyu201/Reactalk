5c2e1b67b38079eb2414889f333c41c1753dbcae
alyu201 <alyu201@aucklanduni.ac.nz>
2022-05-20 12:04:13 +1200

    Show code output to VSCode

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,44 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as commands from "./actions.json";
+
+interface Action {
+  input: String;
+  snippet: String;
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "reactalk" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "reactalk" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('reactalk.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from !');
-	});
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const commandProcess = vscode.commands.registerCommand(
+    "reactalk.commandProcess",
+    () => {
+      // Get the active text editor
+      const editor = vscode.window.activeTextEditor;
 
-	context.subscriptions.push(disposable);
+      if (editor) {
+        const command = commands.composition.filter((cmd: Action) => {
+          return cmd.input === "Add for loop";
+        });
+
+        const snippet = command[0].snippet;
+        console.log(snippet);
+        editor.insertSnippet(new vscode.SnippetString(snippet));
+      }
+
+      vscode.window.showInformationMessage("Processed");
+    }
+  );
+
+  context.subscriptions.push(commandProcess);
 }
 
 // this method is called when your extension is deactivated
