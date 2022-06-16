87fa8c02dfe963e385631a6b978bb6c389fc6445
alyu201 <alyu201@aucklanduni.ac.nz>
2022-06-16 17:26:03 +1200

    Modify nearby element logic

--- src/commandProcessor/processors/editingProcessor.ts
+++ src/commandProcessor/processors/editingProcessor.ts
@@ -5,8 +5,10 @@ import {
 } from "./../../definitions/commandPrefixes";
 import { InvalidCommandException } from "../invalidCommandException";
 import * as vscode from "vscode";
+import { Selection } from "vscode";
 
 const errorMsg = "Error processing editing command";
+const editor = vscode.window.activeTextEditor;
 
 /**
  * @param prefix The prefix of the editing command to process
@@ -26,65 +28,77 @@ export const processEdit = (prefix: string, cmd: string) => {
 };
 
 /**
+ * @param selection The selection from the editor to remove
+ */
+const deleteFromEditor = (selection: Selection) => {
+  if (editor) {
+    editor.edit((builder) => {
+      builder.replace(selection, "");
+    });
+  }
+};
+
+/**
  * @param keyword The keyword used to determine the type of code to delete
  * @param value The value to identify the code to delete by
  * @throws An InvalidCommandException when an error occurs during processing
  */
 const deleteEdit = (keyword: string, value: string) => {
-  let editor = vscode.window.activeTextEditor;
-
   if (editor) {
     switch (keyword) {
       case EditingKeyword.line:
         const range = editor.document.lineAt(parseInt(value) - 1).range;
-        const selection = (editor.selection = new vscode.Selection(
+        const line = (editor.selection = new vscode.Selection(
           range.start,
           range.end
         ));
 
-        editor.edit((builder) => {
-          builder.replace(selection, "");
-        });
+        deleteFromEditor(line);
         break;
       case EditingKeyword.element:
         const documentText = editor.document.getText();
         const endTag = "</p>";
         // if (value in EditingValue) {
+        // Find indices of all occurences of the specified element
         const openingMatches = [
           ...documentText.matchAll(new RegExp("<p>", "gm")),
         ];
         const closingMatches = [
           ...documentText.matchAll(new RegExp(endTag, "gm")),
         ];
-        console.log("openingIdx", openingMatches);
-        console.log("closingIdx", closingMatches);
-        const currentPos = editor.document.offsetAt(editor.selection.active);
-        console.log(currentPos);
+
         let indices: any[] = [];
         openingMatches.forEach((match, index) => {
           if (match.index) {
-            indices[index] = [match.index];
+            if (editor) {
+              indices[index] = [editor.document.positionAt(match.index)];
+            }
           }
         });
         closingMatches.forEach((match, index) => {
           if (match.index) {
-            indices[index] = [...indices[index], match.index];
+            if (editor) {
+              indices[index] = [
+                ...indices[index],
+                editor.document.positionAt(match.index + endTag.length),
+              ];
+            }
           }
         });
-        console.log(indices);
+
+        // Compute the closest occurence
+        const currentPos = editor.selection.active;
         const idx = indices.reduce(function (prev, curr) {
-          return Math.abs(curr[0] - currentPos) <
-            Math.abs(prev[0] - currentPos) ||
-            Math.abs(curr[1] - currentPos) < Math.abs(prev[1] - currentPos)
+          return Math.abs(curr[0]._line - currentPos.line) <
+            Math.abs(prev[1]._line - currentPos.line)
             ? curr
             : prev;
         });
-        console.log(idx);
-        const startPos = editor.document.positionAt(idx[0]);
-        const endPos = editor.document.positionAt(idx[1] + endTag.length);
-        console.log(startPos);
-        console.log(endPos);
-        editor.selection = new vscode.Selection(startPos, endPos);
+        const element = (editor.selection = new vscode.Selection(
+          idx[0],
+          idx[1]
+        ));
+        deleteFromEditor(element);
         // }
         break;
       default:
