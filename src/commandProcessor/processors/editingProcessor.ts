571a3dfc08b488a26dfce16ace412611000f04d5
alyu201 <alyu201@aucklanduni.ac.nz>
2022-06-16 15:54:18 +1200

    Working get closest HTML element to delete logic

--- src/commandProcessor/processors/editingProcessor.ts
+++ src/commandProcessor/processors/editingProcessor.ts
@@ -1,6 +1,7 @@
 import {
   EditingKeyword,
   EditingPrefixes,
+  EditingValue,
 } from "./../../definitions/commandPrefixes";
 import { InvalidCommandException } from "../invalidCommandException";
 import * as vscode from "vscode";
@@ -40,11 +41,51 @@ const deleteEdit = (keyword: string, value: string) => {
           range.start,
           range.end
         ));
-        // editor.revealRange(range);
 
         editor.edit((builder) => {
           builder.replace(selection, "");
         });
+        break;
+      case EditingKeyword.element:
+        const documentText = editor.document.getText();
+        const endTag = "</p>";
+        // if (value in EditingValue) {
+        const openingMatches = [
+          ...documentText.matchAll(new RegExp("<p>", "gm")),
+        ];
+        const closingMatches = [
+          ...documentText.matchAll(new RegExp(endTag, "gm")),
+        ];
+        console.log("openingIdx", openingMatches);
+        console.log("closingIdx", closingMatches);
+        const currentPos = editor.document.offsetAt(editor.selection.active);
+        console.log(currentPos);
+        let indices: any[] = [];
+        openingMatches.forEach((match, index) => {
+          if (match.index) {
+            indices[index] = [match.index];
+          }
+        });
+        closingMatches.forEach((match, index) => {
+          if (match.index) {
+            indices[index] = [...indices[index], match.index];
+          }
+        });
+        console.log(indices);
+        const idx = indices.reduce(function (prev, curr) {
+          return Math.abs(curr[0] - currentPos) <
+            Math.abs(prev[0] - currentPos) ||
+            Math.abs(curr[1] - currentPos) < Math.abs(prev[1] - currentPos)
+            ? curr
+            : prev;
+        });
+        console.log(idx);
+        const startPos = editor.document.positionAt(idx[0]);
+        const endPos = editor.document.positionAt(idx[1] + endTag.length);
+        console.log(startPos);
+        console.log(endPos);
+        editor.selection = new vscode.Selection(startPos, endPos);
+        // }
         break;
       default:
         throw new InvalidCommandException(errorMsg);
