b36ec33a4bb289dcedf611ee01539043f44f9f9d
alyu <68038316+alyu201@users.noreply.github.com>
2022-06-30 22:27:06 +1200

    Editing commands (#10)
    * Show code output to VSCode
    
    * Refactor into modules and add editing processor
    
    * Add navigation processor
    
    * Process navigate to end of line
    
    * Add system processor and comments
    
    * Feature/google speech api (#2)
    
    * Copied in the code for Google Speech API for microphone input from Google Speech API documentation.
    
    * Tried to get the sox to work in VSCode. It is still not working.
    
    * Got the Google Speech API to work.
    
    * Refactored some code and comments.
    
    * Implemented the inputProcessor to pass the pre-processed transcript to a dummy command processor.
    
    * Investigated the Google Speech Error: Exceeded maximum allowed stream duration of 305 seconds.
    
    * Working get closest HTML element to delete logic
    
    * Modify nearby element logic
    
    * Delete html elements commands
    
    * Add error handling
    
    * Refactored editing commands to reduce code size
    
    Co-authored-by: MeganLim97 <90686755+MeganLim97@users.noreply.github.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import { startListening } from './inputProcessor/speechRecognition';
-import { processCommand } from './commandProcessor/commandProcessor';
+import * as vscode from "vscode";
+import { startListening } from "./inputProcessor/speechRecognition";
+import { processCommand } from "./commandProcessor/commandProcessor";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -14,16 +14,16 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('reactalk.helloWorld', () => {
+  let disposable = vscode.commands.registerCommand("reactalk.helloWorld", () => {
     // The code you place here will be executed every time your command is executed
     // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from Megan!');
+    vscode.window.showInformationMessage("Hello World from Megan!");
   });
 
-  let disposable2 = vscode.commands.registerCommand('reactalk.start', () => {
+  let disposable2 = vscode.commands.registerCommand("reactalk.start", () => {
     // The code you place here will be executed every time your command is executed
     // Display a message box to the user
-    vscode.window.showInformationMessage('You can start to talk');
+    vscode.window.showInformationMessage("You can start to talk");
     //startListening();
   });
 
@@ -31,15 +31,15 @@ export function activate(context: vscode.ExtensionContext) {
   // A Disposable type can release resources (like event listening or a timer).
   // When this disposable is disposed, it'll make the associated command become unregistered.
   let disposableStartLis = vscode.commands.registerCommand(
-    'reactalk.startListening',
+    "reactalk.startListening",
     startListening
   );
 
   // TODO: remove this command after completion - for testing purposes only
-  let commandProcess = vscode.commands.registerCommand('reactalk.commandProcess', () => {
-    processCommand('add element container');
+  let commandProcess = vscode.commands.registerCommand("reactalk.commandProcess", () => {
+    processCommand("delete element container");
 
-    vscode.window.showInformationMessage('Processed');
+    vscode.window.showInformationMessage("Processed");
   });
 
   context.subscriptions.push(disposable);
