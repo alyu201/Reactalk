3b31acda8d3f65f4fb55e71364be6cff1d749267
alyu <68038316+alyu201@users.noreply.github.com>
2022-06-30 22:16:25 +1200

    Composition commands (#9)
    * Show code output to VSCode
    
    * Refactor into modules and add editing processor
    
    * Add navigation processor
    
    * Process navigate to end of line
    
    * Add system processor and comments
    
    * Feature/google speech api (#2)
    
    * Copied in the code for Google Speech API for microphone input from Google Speech API documentation.
    
    * Tried to get the sox to work in VSCode. It is still not working.
    
    * Got the Google Speech API to work.
    
    * Refactored some code and comments.
    
    * Implemented the inputProcessor to pass the pre-processed transcript to a dummy command processor.
    
    * Investigated the Google Speech Error: Exceeded maximum allowed stream duration of 305 seconds.
    
    * Add div element command
    
    * Add paragraph and list element commands
    
    * Add heading element commands
    
    * Add anchor and image elements with attribute commands
    
    * Refactored the composition processor to deal with commands with user given code snippets
    
    Co-authored-by: MeganLim97 <90686755+MeganLim97@users.noreply.github.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
-import { startListening } from "./inputProcessor/speechRecognition";
+import * as vscode from 'vscode';
+import { startListening } from './inputProcessor/speechRecognition';
+import { processCommand } from './commandProcessor/commandProcessor';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,19 +14,16 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "reactalk.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from Megan!");
-    }
-  );
-
-  let disposable2 = vscode.commands.registerCommand("reactalk.start", () => {
+  let disposable = vscode.commands.registerCommand('reactalk.helloWorld', () => {
     // The code you place here will be executed every time your command is executed
     // Display a message box to the user
-    vscode.window.showInformationMessage("You can start to talk");
+    vscode.window.showInformationMessage('Hello World from Megan!');
+  });
+
+  let disposable2 = vscode.commands.registerCommand('reactalk.start', () => {
+    // The code you place here will be executed every time your command is executed
+    // Display a message box to the user
+    vscode.window.showInformationMessage('You can start to talk');
     //startListening();
   });
 
@@ -33,13 +31,21 @@ export function activate(context: vscode.ExtensionContext) {
   // A Disposable type can release resources (like event listening or a timer).
   // When this disposable is disposed, it'll make the associated command become unregistered.
   let disposableStartLis = vscode.commands.registerCommand(
-    "reactalk.startListening",
+    'reactalk.startListening',
     startListening
   );
+
+  // TODO: remove this command after completion - for testing purposes only
+  let commandProcess = vscode.commands.registerCommand('reactalk.commandProcess', () => {
+    processCommand('add element container');
+
+    vscode.window.showInformationMessage('Processed');
+  });
 
   context.subscriptions.push(disposable);
   context.subscriptions.push(disposable2);
   context.subscriptions.push(disposableStartLis);
+  context.subscriptions.push(commandProcess);
 }
 
 // this method is called when your extension is deactivated
