34853858ba98ba3e265088a4e9863fc5052e4f14
alyu201 <alyu201@aucklanduni.ac.nz>
2022-08-04 21:46:04 +1200

    Copy variable commands

---
+++ src/commandProcessor/processors/editing/copyLet.ts
@@ -0,0 +1,13 @@
+import * as vscode from "vscode";
+import { camelize, searchEditor } from "../../utility";
+
+const copy = async (name: string) => {
+  await searchEditor(`let ${camelize(name)}`);
+  await vscode.commands.executeCommand("cursorEnd");
+  await vscode.commands.executeCommand("editor.action.clipboardCopyAction");
+  await vscode.window.showInformationMessage("Copied to clipboard!");
+};
+
+export const execute = (name: string) => {
+  copy(name);
+};
