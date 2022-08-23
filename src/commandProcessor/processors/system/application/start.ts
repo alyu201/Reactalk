53c7b4f69efef5dfa33a60d61f140fd25326555c
MeganLim97 <mlim807@aucklanduni.ac.nz>
2022-08-24 00:38:13 +1200

    Refactored the "start application" command.

--- src/commandProcessor/processors/system/application/start.ts
+++ src/commandProcessor/processors/system/application/start.ts
@@ -2,9 +2,12 @@ import * as vscode from "vscode";
 
 export const execute = (sysCmdValue: string) => {
   console.log("sysCmdValue: " + sysCmdValue);
-  const terminal = vscode.window.activeTerminal;
 
-  if (terminal != undefined) {
-    terminal.sendText("npm start");
-  }
+  vscode.commands.executeCommand("terminal.focus").then(function () {
+    const terminal = vscode.window.activeTerminal;
+
+    if (terminal != undefined) {
+      terminal.sendText("npm start");
+    }
+  });
 };
