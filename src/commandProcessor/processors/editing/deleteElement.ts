9efd0bb0a2af811cf6ee94f8365e02ebf15c80ee
alyu201 <alyu201@aucklanduni.ac.nz>
2022-08-24 16:29:12 +1200

    Remove duplicate code and add error handling for  delete and rename element commands

--- src/commandProcessor/processors/editing/deleteElement.ts
+++ src/commandProcessor/processors/editing/deleteElement.ts
@@ -1,33 +1,20 @@
 import * as vscode from "vscode";
 import { EditingValue } from "./../../../definitions/commandPrefixes";
 import { InvalidCommandException } from "../../invalidCommandException";
+import { searchEditor } from "../../utility";
 
-const throwError = () => {
-  throw new InvalidCommandException("Error processing editing command");
-};
-
-const removeElement = async (elem: string) => {
-  await vscode.commands.executeCommand("cursorHome");
-  await vscode.commands.executeCommand("editor.actions.findWithArgs", {
-    isRegex: true,
-    searchString: elem,
-  });
-  await vscode.commands.executeCommand("editor.action.nextMatchFindAction");
-  await vscode.commands.executeCommand("closeFindWidget");
-  await vscode.commands.executeCommand("editor.emmet.action.removeTag");
-};
-
-export const execute = (elem: string) => {
+export const execute = async (elem: string) => {
   const name = elem.split(" ")[0];
   if (Object.keys(EditingValue).includes(name)) {
     const num = elem.split(" ")[1];
-    elem = EditingValue[name as keyof typeof EditingValue].replace(
-      "$",
+    elem = `${EditingValue[name as keyof typeof EditingValue]}`.replace(
+      new RegExp("\\$", "g"),
       name === "heading" ? num : ""
     );
 
-    removeElement(elem);
+    await searchEditor(elem); // throws InvalidCommandException when not found
+    await vscode.commands.executeCommand("editor.emmet.action.removeTag");
   } else {
-    throwError();
+    throw new InvalidCommandException("Error processing editing command");
   }
 };
--- src/commandProcessor/processors/editing/renameElement.ts
+++ src/commandProcessor/processors/editing/renameElement.ts
@@ -1,33 +1,25 @@
 import * as vscode from "vscode";
 import { EditingValue, ElementTags } from "../../../definitions/commandPrefixes";
 import { InvalidCommandException } from "../../invalidCommandException";
+import { searchEditor } from "../../utility";
 
 const throwError = () => {
   throw new InvalidCommandException("Error processing editing command");
 };
 
 const rename = async (name: string, newName: string) => {
-  const prefix = newName.split(" ")[0];
+  const newPrefix = newName.split(" ")[0];
 
   // Find the element to rename by its "name"
-  if (Object.keys(EditingValue).includes(prefix)) {
-    await vscode.commands.executeCommand("cursorHome");
-    await vscode.commands.executeCommand("editor.actions.findWithArgs", {
-      isRegex: true,
-      searchString: name.split("|")[0],
-    });
-    // Jump to the closest, first match
-    await vscode.commands.executeCommand("editor.action.nextMatchFindAction");
-    await vscode.commands.executeCommand("editor.action.previousMatchFindAction");
-
-    await vscode.commands.executeCommand("closeFindWidget");
+  if (Object.keys(EditingValue).includes(newPrefix)) {
+    await searchEditor(name); // throws InvalidCommandException when not found
     await vscode.commands.executeCommand("cursorLeft");
     await vscode.commands.executeCommand("cursorRight");
     await vscode.commands.executeCommand("cursorWordRightSelect");
 
     // Get the new HTML element tag to replace with
     const num = newName.split(" ")[1];
-    const newElem = ElementTags[prefix as keyof typeof ElementTags].replace(
+    const newElem = ElementTags[newPrefix as keyof typeof ElementTags].replace(
       "$",
       name === "heading" ? num : ""
     );
